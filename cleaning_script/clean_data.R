c4c132a70a83e32658cb345f874872dcd62b4c29
Mukund Poddar <mukundpoddar1@gmail.com>
2020-12-08 05:11:42 +0530

    Add new predictors

--- cleaning_script/clean_data.R
+++ cleaning_script/clean_data.R
@@ -30,40 +30,42 @@ names(sui) <- str_replace_all(str_trim(str_replace_all(names(sui), "\\(\\$\\)|\\
 # Roll up data to country-year level
 
 sui_agg <- sui %>% 
-  group_by(country_year) %>% 
+  group_by(country, year) %>% 
   summarise(
     suicides_no  = sum(suicides_no, na.rm = T),
     population  = sum(population, na.rm = T),
-    gdp_for_year = unique(gdp_for_year, na.rm = T)
+    gdp_for_year = unique(gdp_for_year, na.rm = T),
+    gdp_per_capita = unique(gdp_per_capita, na.rm = T)
   ) %>% ungroup()
+sui_agg <- sui_agg %>% group_by(country) %>% 
+  mutate(population_change = population - lag(population),
+         gdp_per_capita_change = (gdp_per_capita - lag(gdp_per_capita))/gdp_per_capita) %>% 
+  ungroup()
 
-age_dist <- sui %>% group_by(country_year, age) %>% 
+age_dist <- sui %>% group_by(country, year, age) %>% 
   summarise(population = sum(population, na.rm=T)) %>%
   spread(age,population)
 age_dist$total_pop <- rowSums(age_dist[unique(sui$age)])
 age_dist <- age_dist %>%
-  mutate_at(vars(-total_pop,-country_year), funs(. / total_pop)) %>%
-  select(everything(),-total_pop)
+  mutate_at(vars(-total_pop,-country, -year), funs(. / total_pop)) %>%
+  select(everything(),-total_pop) %>% ungroup()
+age_dist <- age_dist %>% mutate(mean_age = 10*.$`5-14 years`+20*.$`15-24 years`+30*.$`25-34 years`
+                                +45*.$`35-54 years` + 65*.$`55-74 years` + 85*.$`75+ years`)
 
-sex_dist <- sui %>% group_by(country_year, sex) %>% 
+sex_dist <- sui %>% group_by(country, year, sex) %>% 
   summarise(population = sum(population, na.rm=T)) %>%
   spread(sex,population)
 sex_dist$total_pop <- rowSums(sex_dist[unique(sui$sex)])
 sex_dist <- sex_dist %>%
-  mutate_at(vars(-total_pop,-country_year), funs(. / total_pop)) %>%
+  mutate_at(vars(-total_pop,-country, -year), funs(. / total_pop)) %>%
   select(everything(),-total_pop)
+
 
 # DATA CLEANING -----------------------------------------------------------
 
-#split country and year, then add them in as 2 new columns
-country_year_split <- strsplit(sui_agg$country_year,
-                               "(?<=[A-Za-z])\\s*(?=\\d+$)", perl = TRUE)
-sui_agg$country <- sapply(country_year_split, "[", 1)
-sui_agg$year <- sapply(country_year_split, "[", 2)
-
 #join in age and sex distributions
 
-sui_agg <- sui_agg %>% left_join(age_dist) %>% left_join(sex_dist) %>% select(everything(),-male)
+sui_agg <- sui_agg %>% left_join(age_dist, by=c('country', 'year')) %>% left_join(sex_dist, by=c('country', 'year')) %>% select(everything(),-male)
 
 # OUTPUT DATA -------------------------------------------------------------
 write_csv(sui_agg, "../clean_data/suicide_data.csv")
